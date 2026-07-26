0d6e13c4ceb6ae91bf6b183b4eac2cbf5209d7b8
Rdazochka <rdazochka@gmail.com>
2026-07-26 21:44:06 +0300

    fix components

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-import App from "./Components/App/App";
+import App from "./components/App/App";
 
 const queryClient = new QueryClient();
 
